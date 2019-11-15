6f5e2af9d712daf650b4a122e81a6ef004218390
mattshardman <mattshardman@gmail.com>
2019-11-15 17:06:34 +0800

    load function added

--- cpu.py
+++ cpu.py
@@ -1,3 +1,5 @@
+import sys
+
 class CPU:
     def __init__(self):
         self.pc = 1
@@ -8,3 +10,17 @@ class CPU:
         self.reg[7] = 0xf4
         self.sp = self.reg[7]
 
+    def load(self):
+        f = open(sys.argv[1], "r")
+        address = 0
+        for line in f:
+            ln = ''
+            for char in line:
+                if char == '#':
+                    break
+                if char == '\n':
+                    break
+                ln += char
+            if len(ln) > 0:
+                self.ram[address] = int(ln, 2)
+                address += 1
